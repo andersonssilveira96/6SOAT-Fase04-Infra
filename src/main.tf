297771d5d8d31040d1c7a7b23dbc91a2b9afb619
Anderson Souza <andersonssilveira96@gmail.com>
2024-11-27 22:35:57 -0300

    Adding mongoDB

--- src/main.tf
+++ src/main.tf
@@ -1,3 +1,74 @@
 provider "aws" {
  region = "us-east-1"
 }
+
+terraform {
+  required_providers {
+    mongodbatlas = {
+      source = "mongodb/mongodbatlas"
+      version = "~> 1.7"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "mongodbatlas" {
+  public_key  = var.atlas_public_key
+  private_key = var.atlas_private_key
+}
+
+resource "mongodbatlas_project" "project" {
+  name = var.project_name
+}
+
+resource "mongodbatlas_cluster" "cluster" {
+  project_id   = mongodbatlas_project.project.id
+  name         = var.cluster_name
+  provider_name = "AWS"
+  provider_region_name = var.region
+  cluster_type = "SHARDED"
+  mongo_db_major_version = "6.0"
+
+  replication_specs {
+    num_shards = 1
+    regions_config {
+      region_name     = var.region
+      electable_nodes = 3
+      priority        = 7
+      read_only_nodes = 0
+    }
+  }
+
+  disk_size_gb           = 10
+  backup_enabled         = true
+}
+
+resource "mongodbatlas_database_user" "db_user" {
+  project_id    = mongodbatlas_project.project.id
+  username      = var.db_username
+  password      = var.db_password
+  auth_database_name = "admin"
+
+  roles {
+    role_name     = "readWriteAnyDatabase"
+    database_name = "admin"
+  }
+
+  scopes {
+    type = "CLUSTER"
+    name = mongodbatlas_cluster.cluster.name
+  }
+}
+
+resource "mongodbatlas_network_container" "network" {
+  project_id      = mongodbatlas_project.project.id
+  atlas_cidr_block = "192.168.0.0/24"
+  provider_name   = "AWS"
+  region_name     = var.region
+}
+
+resource "mongodbatlas_project_ip_whitelist" "whitelist" {
+  project_id = mongodbatlas_project.project.id
+  ip_address = "0.0.0.0/0" # Permite acesso de todos os IPs
+}
+
--- src/variables.tf
+++ src/variables.tf
@@ -13,3 +13,40 @@ variable "db_password" {
 variable "db_name" {
   default = "techchallenge"
 }
+
+variable "atlas_public_key" {
+  description = "Public key for MongoDB Atlas API."
+  type        = string
+}
+
+variable "atlas_private_key" {
+  description = "Private key for MongoDB Atlas API."
+  type        = string
+}
+
+variable "project_name" {
+  description = "Name of the MongoDB Atlas project."
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Name of the MongoDB Atlas cluster."
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region for MongoDB Atlas cluster."
+  type        = string
+}
+
+variable "db_username" {
+  description = "Username for MongoDB database user."
+  type        = string
+}
+
+variable "db_password" {
+  description = "Password for MongoDB database user."
+  type        = string
+  sensitive   = true
+}
+
